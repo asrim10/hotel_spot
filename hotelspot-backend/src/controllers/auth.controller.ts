fafa7a25a852c844fe0ea68bfc1e9ef4fa0c2805
asrim10 <asrimmsuwall@gmail.com>
2026-01-04 21:47:35 +0545

    authController_for_loginUser

--- hotelspot-backend/src/controllers/auth.controller.ts
+++ hotelspot-backend/src/controllers/auth.controller.ts
@@ -27,4 +27,28 @@ export class AuthController {
       });
     }
   }
+  async login(req: Request, res: Response) {
+    try {
+      const parsedData = LoginUserDTO.safeParse(req.body);
+      if (!parsedData.success) {
+        return res.status(400).json({
+          success: false,
+          message: z.prettifyError(parsedData.error),
+        });
+      }
+      const loginData: LoginUserDTO = parsedData.data;
+      const { token, user } = await userService.loginUser(loginData);
+      return res.status(200).json({
+        success: true,
+        message: "Login Successful",
+        data: user,
+        token,
+      });
+    } catch (error: Error | any) {
+      return res.status(error.statusCode ?? 500).json({
+        success: false,
+        message: error.message || "Internal Service Error",
+      });
+    }
+  }
 }
