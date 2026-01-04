0bc0b16a14b19788285f2dab6b5ef1525b6199be
asrim10 <asrimmsuwall@gmail.com>
2026-01-04 21:44:13 +0545

    userService_for_loginUser

--- hotelspot-backend/src/services/user.service.ts
+++ hotelspot-backend/src/services/user.service.ts
@@ -27,4 +27,26 @@ export class UserService {
     const newUser = await userRepositoory.createUser(data);
     return newUser;
   }
+  async loginUser(data: LoginUserDTO) {
+    const user = await userRepositoory.getUserByEmail(data.email);
+    if (!user) {
+      throw new HttpError(404, "User not found");
+    }
+    const validPassword = await bcryptjs.compare(data.password, user.password);
+    if (!validPassword) {
+      throw new HttpError(401, "Invalid credentials");
+    }
+    //generate jwt
+    const payload = {
+      id: user._id,
+      email: user.email,
+      username: user.email,
+      password: user.password,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      role: user.role,
+    };
+    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "30d" }); // 30days
+    return { token, user };
+  }
 }
