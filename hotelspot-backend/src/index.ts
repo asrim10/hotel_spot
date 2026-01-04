ab1a1fa3300ec247113cb2e3acccf54457ff6b74
asrim10 <asrimmsuwall@gmail.com>
2026-01-04 19:45:32 +0545

    database_connection

--- hotelspot-backend/src/index.ts
+++ hotelspot-backend/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Application, Request, Response } from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import { connectDatabase } from "./database/mongodb";
+import { PORT } from "./config";
 dotenv.config();
 
 console.log(process.env.PORT);
@@ -11,3 +13,11 @@ app.use(bodyParser.json());
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
+
+async function startServer() {
+  await connectDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server: http://localhost:${PORT}`);
+  });
+}
+startServer();
