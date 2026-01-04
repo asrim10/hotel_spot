964c7f0ad348c4f78ff4a8ea658b75ff9ff89c67
asrim10 <asrimmsuwall@gmail.com>
2026-01-04 19:44:27 +0545

    config_index.ts

---
+++ hotelspot-backend/src/config/index.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const PORT: number = process.env.PORT
+  ? parseInt(process.env.PORT)
+  : 3000;
+export const MONGODB_URI: string =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/defaultdb";
+
+//Application level constant, with fallbacks
+// if .env variables are not set
+
+export const JWT_SECRET: string = process.env.JWT_SECRET || "default";
