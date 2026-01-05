fc2adf7830b1643513e4f211817953b75ce7f3ed
asrim10 <asrimmsuwall@gmail.com>
2026-01-05 07:53:37 +0545

    authController_updated_with_update_and_delete

--- hotelspot-backend/src/controllers/auth.controller.ts
+++ hotelspot-backend/src/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import { UserService } from "../services/user.service";
-import { CreateUserDTO, LoginUserDTO } from "../dtos/user.dto";
+import { CreateUserDTO, LoginUserDTO, UpdateUserDTO } from "../dtos/user.dto";
 import { Request, Response } from "express";
 import z, { success } from "zod";
 
@@ -45,6 +45,51 @@ export class AuthController {
         token,
       });
     } catch (error: Error | any) {
+      return res.status(error.statusCode ?? 500).json({
+        success: false,
+        message: error.message || "Internal Service Error",
+      });
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    try {
+      const userId = req.params.id;
+
+      const parsedData = UpdateUserDTO.safeParse(req.body);
+      if (!parsedData.success) {
+        return res.status(400).json({
+          success: false,
+          message: z.prettifyError(parsedData.error),
+        });
+      }
+
+      const updatedUser = await userService.updateUser(userId, parsedData.data);
+
+      return res.status(200).json({
+        success: true,
+        message: "User updated successfully",
+        data: updatedUser,
+      });
+    } catch (error: any) {
+      return res.status(error.statusCode ?? 500).json({
+        success: false,
+        message: error.message || "Internal Service Error",
+      });
+    }
+  }
+
+  async delete(req: Request, res: Response) {
+    try {
+      const userId = req.params.id;
+
+      const result = await userService.deleteUser(userId);
+
+      return res.status(200).json({
+        success: true,
+        message: result.message,
+      });
+    } catch (error: any) {
       return res.status(error.statusCode ?? 500).json({
         success: false,
         message: error.message || "Internal Service Error",
