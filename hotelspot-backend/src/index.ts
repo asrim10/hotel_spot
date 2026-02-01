ec7fa1c33948469044d3922f45f8941914b2e537
asrim10 <asrimmsuwall@gmail.com>
2026-02-01 09:03:08 +0545

    admin_routes_,_uploads_and_error addded

--- hotelspot-backend/src/index.ts
+++ hotelspot-backend/src/index.ts
@@ -4,9 +4,11 @@ import dotenv from "dotenv";
 import { connectDatabase } from "./database/mongodb";
 import { PORT } from "./config";
 import cors from "cors";
+import { HttpError } from "./errors/http-error";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes";
-import adminRoutes from "./routes/admin/user.routes";
+import adminUserRoutes from "./routes/admin/user.routes";
 dotenv.config();
 
 console.log(process.env.PORT);
@@ -16,21 +18,35 @@ const app: Application = express();
 const corsOptions = {
   origin: [
     "http://localhost:3000",
-    "http://localhost:4000",
+    "http://localhost:3003",
     "http://localhost:3005",
   ],
   optionsSuccessStatus: 200,
   credentials: true,
 };
+
 app.use(cors(corsOptions));
+
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 app.use(bodyParser.json());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/admin/users", adminRoutes);
+app.use("/api/admin/users", adminUserRoutes);
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
+  res.send("Welcome to API World!");
+});
+
+app.use((err: Error, req: Request, res: Response, next: Function) => {
+  if (err instanceof HttpError) {
+    return res
+      .status(err.statusCode)
+      .json({ success: false, message: err.message });
+  }
+  return res
+    .status(500)
+    .json({ success: false, message: err.message || "Internal Server Error" });
 });
 
 async function startServer() {
