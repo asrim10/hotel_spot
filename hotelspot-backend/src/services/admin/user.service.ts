1afd7997eb02c2d274a9a11704ce4adbe8e80afc
asrim10 <asrimmsuwall@gmail.com>
2026-02-01 08:53:01 +0545

    admin/_user_service

--- hotelspot-backend/src/services/admin/user.service.ts
+++ hotelspot-backend/src/services/admin/user.service.ts
@@ -1,6 +1,10 @@
+import {
+  CreateUserDTO,
+  LoginUserDTO,
+  UpdateUserDTO,
+} from "../../dtos/user.dto";
 import { UserRepository } from "../../repositories/user.repositories";
 import bcryptjs from "bcryptjs";
-import { CreateUserDTO } from "../../dtos/user.dto";
 import { HttpError } from "../../errors/http-error";
 
 let userRepository = new UserRepository();
@@ -15,7 +19,8 @@ export class AdminUserService {
     if (usernameCheck) {
       throw new HttpError(403, "Username already in use");
     }
-    const hashedPassword = await bcryptjs.hash(data.password, 10); //10 complexity
+    // hash password
+    const hashedPassword = await bcryptjs.hash(data.password, 10); // 10 - complexity
     data.password = hashedPassword;
 
     const newUser = await userRepository.createUser(data);
@@ -26,9 +31,27 @@ export class AdminUserService {
     const users = await userRepository.getAllUsers();
     return users;
   }
-  async getOneUser(id: string) {
+
+  async deleteUser(id: string) {
     const user = await userRepository.getUserByID(id);
-    console.log(user);
+    if (!user) {
+      throw new HttpError(404, "User not found");
+    }
+    const deleted = await userRepository.deleteUserById(id);
+    return deleted;
+  }
+
+  async updateUser(id: string, updateData: UpdateUserDTO) {
+    const user = await userRepository.getUserByID(id);
+    if (!user) {
+      throw new HttpError(404, "User not found");
+    }
+    const updatedUser = await userRepository.updateUser(id, updateData);
+    return updatedUser;
+  }
+
+  async getUserById(id: string) {
+    const user = await userRepository.getUserByID(id);
     if (!user) {
       throw new HttpError(404, "User not found");
     }
