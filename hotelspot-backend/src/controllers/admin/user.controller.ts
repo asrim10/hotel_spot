0183c8ca55816b4006ab9fbd8b09893ae3c84c4c
asrim10 <asrimmsuwall@gmail.com>
2026-02-01 08:56:17 +0545

    admin_user_controller

--- hotelspot-backend/src/controllers/admin/user.controller.ts
+++ hotelspot-backend/src/controllers/admin/user.controller.ts
@@ -1,29 +1,30 @@
-import { CreateUserDTO, UpdateUserDTO } from "../../dtos/user.dto";
-import { AdminUserService } from "../../services/admin/user.service";
-import { Request, Response } from "express";
+import {
+  CreateUserDTO,
+  LoginUserDTO,
+  UpdateUserDTO,
+} from "../../dtos/user.dto";
+import { Request, Response, NextFunction } from "express";
 import z from "zod";
+import { AdminUserService } from "../../services/admin/user.service";
 
-const adminUserService = new AdminUserService();
+let adminUserService = new AdminUserService();
 
 export class AdminUserController {
-  async createUser(req: Request, res: Response) {
+  async createUser(req: Request, res: Response, next: NextFunction) {
     try {
-      const parsedData = CreateUserDTO.safeParse(req.body);
+      const parsedData = CreateUserDTO.safeParse(req.body); // validate request body
       if (!parsedData.success) {
-        return res.status(400).json({
-          success: false,
-          message: z.prettifyError(parsedData.error),
-        });
+        // validation failed
+        return res
+          .status(400)
+          .json({ success: false, message: z.prettifyError(parsedData.error) });
       }
-
-      const newUser = await adminUserService.createUser(parsedData.data);
-
-      return res.status(201).json({
-        success: true,
-        message: "User created successfully",
-        data: newUser,
-      });
-    } catch (error: any) {
+      const userData: CreateUserDTO = parsedData.data;
+      const newUser = await adminUserService.createUser(userData);
+      return res
+        .status(201)
+        .json({ success: true, message: "User Created", data: newUser });
+    } catch (error: Error | any) {
       return res.status(error.statusCode ?? 500).json({
         success: false,
         message: error.message || "Internal Server Error",
@@ -31,33 +32,69 @@ export class AdminUserController {
     }
   }
 
-  async getOneUser(req: Request, res: Response) {
-    try {
-      const userId = req.params.id;
-
-      const user = await adminUserService.getOneUser(userId);
-
-      return res.status(200).json({
-        success: true,
-        data: user,
-      });
-    } catch (error: any) {
-      return res.status(error.statusCode ?? 500).json({
-        success: false,
-        message: error.message || "Internal Server Error",
-      });
-    }
-  }
-
-  async getAllUsers(req: Request, res: Response) {
+  async getAllUsers(req: Request, res: Response, next: NextFunction) {
     try {
       const users = await adminUserService.getAllUsers();
-
-      return res.status(200).json({
-        success: true,
-        data: users,
+      return res
+        .status(200)
+        .json({ success: true, data: users, message: "All Users Retrieved" });
+    } catch (error: Error | any) {
+      return res.status(error.statusCode ?? 500).json({
+        success: false,
+        message: error.message || "Internal Server Error",
       });
-    } catch (error: any) {
+    }
+  }
+
+  async updateUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      const userId = req.params.id;
+      const parsedData = UpdateUserDTO.safeParse(req.body); // validate request body
+      if (!parsedData.success) {
+        // validation failed
+        return res
+          .status(400)
+          .json({ success: false, message: z.prettifyError(parsedData.error) });
+      }
+      const updateData: UpdateUserDTO = parsedData.data;
+      const updatedUser = await adminUserService.updateUser(userId, updateData);
+      return res
+        .status(200)
+        .json({ success: true, message: "User Updated", data: updatedUser });
+    } catch (error: Error | any) {
+      return res.status(error.statusCode ?? 500).json({
+        success: false,
+        message: error.message || "Internal Server Error",
+      });
+    }
+  }
+
+  async deleteUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      const userId = req.params.id;
+      const deleted = await adminUserService.deleteUser(userId);
+      if (!deleted) {
+        return res
+          .status(404)
+          .json({ success: false, message: "User not found" });
+      }
+      return res.status(200).json({ success: true, message: "User Deleted" });
+    } catch (error: Error | any) {
+      return res.status(error.statusCode ?? 500).json({
+        success: false,
+        message: error.message || "Internal Server Error",
+      });
+    }
+  }
+
+  async getUserById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const userId = req.params.id;
+      const user = await adminUserService.getUserById(userId);
+      return res
+        .status(200)
+        .json({ success: true, data: user, message: "Single User Retrieved" });
+    } catch (error: Error | any) {
       return res.status(error.statusCode ?? 500).json({
         success: false,
         message: error.message || "Internal Server Error",
