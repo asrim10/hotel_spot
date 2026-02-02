e0eb33a871b589e70de76484ff2daa8df2ba4638
asrim10 <asrimmsuwall@gmail.com>
2026-02-02 10:38:06 +0545

    update_user_and_axios_auth

--- hotelspot-frontend/lib/api/auth.ts
+++ hotelspot-frontend/lib/api/auth.ts
@@ -8,7 +8,7 @@ export const register = async (registerData: RegisterData) => {
     return response.data;
   } catch (error: Error | any) {
     throw new Error(
-      error.response?.data?.message || error.message || "Registration failed"
+      error.response?.data?.message || error.message || "Registration failed",
     );
   }
 };
@@ -19,7 +19,32 @@ export const login = async (loginData: LoginData) => {
     return response.data;
   } catch (error: Error | any) {
     throw new Error(
-      error.response?.data?.message || error.message || "Login failed"
+      error.response?.data?.message || error.message || "Login failed",
+    );
+  }
+};
+export const whoAmI = async () => {
+  try {
+    const response = await axios.get(API.AUTH.WHOAMI);
+    return response.data;
+  } catch (error: Error | any) {
+    throw new Error(
+      error.response?.data?.message || error.message || "Whoami failed",
+    );
+  }
+};
+
+export const updateProfile = async (profileData: any) => {
+  try {
+    const response = await axios.put(API.AUTH.UPDATEPROFILE, profileData, {
+      headers: {
+        "Content-Type": "multipart/form-data", // for file upload/multer
+      },
+    });
+    return response.data;
+  } catch (error: Error | any) {
+    throw new Error(
+      error.response?.data?.message || error.message || "Update profile failed",
     );
   }
 };
--- hotelspot-frontend/lib/api/endpoints.ts
+++ hotelspot-frontend/lib/api/endpoints.ts
@@ -2,6 +2,8 @@ export const API = {
   AUTH: {
     LOGIN: "/api/auth/login",
     REGISTER: "/api/auth/register",
+    WHOAMI: "/api/auth/whoami",
+    UPDATEPROFILE: "/api/auth/update-profile",
   },
   ADMIN: {
     USER: {
