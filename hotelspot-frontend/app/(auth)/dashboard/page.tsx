9251c2c2ef8e022c779d6e307c6b7f9db83d055e
asrim10 <asrimmsuwall@gmail.com>
2026-02-02 20:38:26 +0545

    admin_page

--- hotelspot-frontend/app/(auth)/dashboard/page.tsx
+++ hotelspot-frontend/app/(auth)/dashboard/page.tsx
@@ -1,3 +1,13 @@
-export default function Page() {
-  return <div className="text-black bg-amber-50 font-bold">Dashboard Page</div>;
+"use client"; // component must be client when using context
+import { useAuth } from "@/app/context/AuthContext";
+import Image from "next/image";
+
+export default function Home() {
+  const { user, logout } = useAuth();
+  return (
+    <div>
+      HomePage {user ? `Welcome, ${user.email}` : "Not logged in"}
+      <div>{user && <button onClick={logout}>Logout</button>}</div>
+    </div>
+  );
 }
