d19b323ad70eee75866ae12822e80d021f4a8ae5
asrim10 <asrimmsuwall@gmail.com>
2026-03-06 00:59:41 +0545

    fixed_date_in_booking

--- hotelspot-frontend/app/admin/bookings/layout.tsx
+++ hotelspot-frontend/app/admin/bookings/layout.tsx
@@ -1,16 +1,9 @@
 "use client";
 
-import { ToastContainer } from "react-toastify";
-
 export default function BookingsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <>
-      {children}
-      <ToastContainer position="top-right" autoClose={3000} />
-    </>
-  );
+  return <>{children}</>;
 }
